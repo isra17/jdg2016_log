28a0325c54b6a0711f9c7b0acc6a692f0dcc6aa4
isra17 <isra017@gmail.com>
2016-01-02 18:50:08 -0500

    Add instructions

--- solution.java
+++ solution.java
@@ -1,5 +1,19 @@
 import java.io.*;
 
+/*
+ * Modifiez ce fichier si votre solution est en Java.
+ * Ce programme est un court exemple du protocol texte et d'une réponse au
+ * premier challenge "Ping".
+ * Modifiez "run.sh" pour compiler et exécuter ce programme java et
+ * exécutez la suite de test avec la commande "./jdg2016 ./run.sh"
+ *
+ * Pour l'exemple en python regardez "solution.py". Pour d'autre langage,
+ * faites un programme qui lit et écrit par les entrées et sorties standard
+ * et modifier "run.sh" pour compiler et exécuter votre programme.
+ *
+ * N'OUBLIER PAS DE "FLUSHER" VOS SORTIES!
+ */
+
 public class solution {
   public static void main(String[] args) {
     try{
